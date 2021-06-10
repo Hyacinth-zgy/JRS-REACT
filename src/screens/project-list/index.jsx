16bc439a8250a0c79df1f24654e7b243cf1dd503
Hyacinth-zgy <838673371@qq.com>
2021-06-10 17:05:42 +0800

    search-params所需数据

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,10 @@
+import { SearchPannel } from "./search-pannel";
+import { List } from "./list";
+export const ProjectListScreen = () => {
+  return (
+    <div>
+      <SearchPannel></SearchPannel>
+      <List></List>
+    </div>
+  );
+};
