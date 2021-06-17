b9eb27f12ff86961c94f938a907cac981667a58d
Hyacinth-zgy <838673371@qq.com>
2021-06-17 17:31:35 +0800

    完成了自定义的主题

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { useAuth } from "context/auth-context";
 import { AuthenticatedApp } from "./authenticated-app";
 import { UnauthenticatedApp } from "unauthenticated-app/index";
+import { Button } from "antd";
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
+      <Button>sdasd</Button>
       {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
