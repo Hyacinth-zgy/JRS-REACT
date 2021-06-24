89dc5e59038eef310fff1e9fc90c4583ebc68ad9
Hyacinth-zgy <838673371@qq.com>
2021-06-24 20:40:53 +0800

    登录ant改造

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import "./App.css";
 import { useAuth } from "context/auth-context";
 import { AuthenticatedApp } from "./authenticated-app";
 import { UnauthenticatedApp } from "unauthenticated-app/index";
-import { Button } from "antd";
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      <Button>sdasd</Button>
       {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,25 +1,46 @@
 import React from "react";
 import { useAuth } from "context/auth-context";
+import { Form, Input, Button } from "antd";
 export const RegisterScreen = () => {
-  const { user, register } = useAuth();
-  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    const username = (evt.currentTarget.elements[0] as HTMLInputElement).value;
-    const password = (evt.currentTarget.elements[1] as HTMLInputElement).value;
-    register({ username, password });
+  const { register } = useAuth();
+  // 原生的做法
+  // const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+  //   evt.preventDefault();
+  //   const username = (evt.currentTarget.elements[0] as HTMLInputElement).value;
+  //   const password = (evt.currentTarget.elements[1] as HTMLInputElement).value;
+  //   register({ username, password });
+  // };
+  const handlSubmit = (values: {
+    username: string;
+    password: string;
+    cpassword: string;
+  }) => {
+    register(values);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      登录成功:{user?.name}
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id={"username"} />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="text" id="password" />
-      </div>
-      <button type="submit">注册</button>
-    </form>
+    <Form onFinish={handlSubmit}>
+      <Form.Item
+        name={"username"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        <Input placeholder={"用户名"} type="text" id={"username"} />
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[{ required: true, message: "请输入密码" }]}
+      >
+        <Input placeholder={"密码"} type="password" id={"password"} />
+      </Form.Item>
+      <Form.Item
+        name={"cpassword"}
+        rules={[{ required: true, message: "请确认密码" }]}
+      >
+        <Input placeholder={"确认密码"} type="password" id={"cpassword"} />
+      </Form.Item>
+
+      <Button htmlType={"submit"} type={"primary"}>
+        注册
+      </Button>
+    </Form>
   );
 };
